0daa027f7b1a26a79688fa83a73be5f71c5bffdc
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-29 21:17:37 -0800

    header

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var path = require('path');
+var os = require('os');
 var app = express();
 var port = process.env.PORT || 8080;
 console.log('listening on port ' + port);
-
-app.get('/', function(req, res) {
-	res.send('Hello!');
-});
-
-app.listen('3000');
-
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-
-
-    res.send('Hello!');
+	var a = {"ipaddress": req.ip, "language":  req.headers["accept-language"],
+	"software": os.platform()};
+	res.send(a);
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running');
 });
