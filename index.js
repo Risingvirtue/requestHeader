fd2c6f3b999f3281cc508fd8872c3ca0d3028372
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-29 20:36:01 -0800

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var path = require('path');
+var app = express();
+console.log('listening on port 3000');
+
+app.get('/', function(req, res) {
+	res.send('Hello!');
+});
+
+app.listen('3000');
