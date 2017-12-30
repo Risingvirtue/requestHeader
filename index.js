19a38bf65e74313c869015f6987c317701825cfd
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-29 20:41:54 -0800

    heroku vs

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-console.log('listening on port 3000');
+var port = process.env.PORT || 8080;
+console.log('listening on port ' + port);
 
 app.get('/', function(req, res) {
 	res.send('Hello!');
 });
 
 app.listen('3000');
+
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+
+    res.send('Hello!');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
